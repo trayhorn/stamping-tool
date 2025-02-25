20d1eedf0e9ebd307301f8726dfd5e7ef9539e11
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-02-26 01:59:19 +0200

    connected and set up react-pdf library

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
+import { useState } from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+	"pdfjs-dist/build/pdf.worker.min.mjs",
+	import.meta.url
+).toString();
 
 function App() {
+  const [numPages, setNumPages] = useState<number>();
+	const [pageNumber, setPageNumber] = useState<number>(1);
+
+	function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+		setNumPages(numPages);
+  }
+
   return (
-    <>
-      <Header />
-    </>
-  )
+		<>
+			<Header />
+
+			<main>
+				{/* Stamps */}
+
+				<div className="stamps-container">
+					<h2 className="header">Stamps</h2>
+				</div>
+
+				{/* Main part  */}
+				<div>
+					<Document
+						file="../public/sample.pdf"
+						onLoadSuccess={onDocumentLoadSuccess}
+					>
+						<Page
+							renderTextLayer={false}
+							renderAnnotationLayer={false}
+							pageNumber={pageNumber}
+						/>
+					</Document>
+					<p>
+						<button onClick={() => setPageNumber((prev) => prev - 1)}>
+							back
+						</button>
+						<button onClick={() => setPageNumber((prev) => prev + 1)}>
+							forward
+						</button>
+						Page {pageNumber} of {numPages}
+					</p>
+				</div>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
